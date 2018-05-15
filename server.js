1aa22e2f821825e3159f79c3abf738768c7a752d
Bibi <bibi.samreny@protonmail.com>
2018-05-14 19:14:30 -0700

    trying hello world again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-//app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
 
 function runServer() {
   const port = process.env.PORT || 8080;
