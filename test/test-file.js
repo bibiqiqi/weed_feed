3303c87e14fbbc47a909e892cf84c2d6fbf46844
Bibi <bibi.samreny@protonmail.com>
2018-05-14 18:10:55 -0700

    first travis build push

---
+++ test/test-file.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Root', function() {
+  before(function() {
+  return runServer();
+});
+after(function() {
+  return closeServer();
+});
+
+  it('should return a 200 status code', function() {
+    return chai.request(app)
+    .get('/')
+    .then(function(res) {
+      expect(res).to.have.status(200);
+    });
+  });
+});
