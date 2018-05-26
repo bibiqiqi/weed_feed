c48f8c08055cbe9d31dc18d3f81e3124c8b0859a
Bibi <bibi.samreny@protonmail.com>
2018-05-25 22:38:38 -0700

    image grid working

--- public/app.js
+++ public/app.js
@@ -1,106 +1,212 @@
-//for grow-collection-page, where user is able to see a grid of photos that represent their current and past grows
-function getMockData(callback) {
+const GROW_ENTRIES_JSON = 'mock-grow-entries.json';
+let LOCAL_DATA = {};
+let CURRENT_GROW = {};
+let GRID_TRACKER = 0;
 
-//this function is just using a timeout function until I've built a live API,
-//when i'll change it to an AJAX call
-setTimeout(function(){callback(growCollection)}, 1);
+//consolidate ^these into 1 object
+
+//function getAllGrows() {
+//  return new Promise ((resolve, reject) => {
+//    const settings = {
+    //    url: GROW_ENTRIES_JSON,
+    //    dataType: 'json',
+    //    type: 'GET',
+    //    success: callback
+    //  };
+    //  $.ajax(settings);
+//    timeout(100).then
+//    resolve("success!");//fulfilled
+//    reject("total failure");//rejected
+//  });
+//}
+
+
+//function getAllGrows(callback) {
+//  const settings = {
+//    url: GROW_ENTRIES_JSON,
+//    dataType: 'json',
+//    type: 'GET',
+//    success: callback
+//  };
+//  $.ajax(settings);
+//setTimeout(function(){callback(growCollection)}, 100);
+//LOCAL_DATA = growCollection;
+//}
+
+function getAllEntries(callback) {
+//  const settings = {
+//    url: GROW_ENTRIES_JSON,
+//    dataType: 'json',
+//    type: 'GET',
+//    success: callback
+//  };
+//  $.ajax(settings);
+setTimeout(function(){callback(growCollection)}, 100);
 }
 
-function postToMockData(callback) {
-
-//this function is just using a timeout function until I've built a live API,
-//when i'll change it to an AJAX call
-setTimeout(function(){callback(growCollection)}, 1);
+//flip the callback and id arguments
+function getGrow(callback, id) {
+  //  const settings = {
+  //    url: GROW_ENTRIES_JSON/${id},
+  //    dataType: 'json',
+  //    type: 'GET',
+  //    success: callback
+  //  };
+  //  $.ajax(settings);
+  setTimeout(function(){callback(oneGrow)}, 100);
 }
 
-function displayGrowGrid(data) {
-  let numberOfGrows = data.grows.length;
-  renderImageGridHtml(renderImageHtml(), numberOfGrows);
+//flip the callback and id arguments
+function getEntry(callback, id) {
+  //  const settings = {
+  //    url: GROW_ENTRIES_JSON/${id},
+  //    dataType: 'json',
+  //    type: 'GET',
+  //    success: callback
+  //  };
+  //  $.ajax(settings);
+  setTimeout(function(){callback(oneGrow)}, 100);
+}
+
+function postGrow(callback, postObject) {
+  //  const settings = {
+  //    url: GROW_ENTRIES_JSON/${id},
+  //    dataType: 'json',
+  //    type: 'POST',
+  //    data: postObject,
+  //    success: callback
+  //  };
+  //  $.ajax(settings);
+  setTimeout(function(){callback(postGrow)}, 100);
+  postGrow = postObject;
+}
+
+function afterPostTest(data) {
+  console.log(data);
+}
+
+function postEntry(callback, postObject) {
+  //  const settings = {
+  //    url: GROW_ENTRIES_JSON/${id},
+  //    dataType: 'json',
+  //    type: 'POST',
+  //    data: postObject,
+  //    success: callback
+  //  };
+  //  $.ajax(settings);
+  setTimeout(function(){callback(postEntry)}, 100);
+  postEntry = postObject;
+}
+
+function deleteThings (callback, id) {
+//  const settings = {
+//      url: GROW_ENTRIES_JSON/${id},
+//      dataType: 'json',
+//      type: 'DELETE',
+//      success: function(result) {
+//      }
+//  });
+//$.ajax(settings);
+  setTimeout(function(){callback(deleteAgrow)}, 100);
+  deleteAgrow.splice(index, 1);
+}
+
+function afterDeleteTest(data) {
+  console.log(deleteAgrow);
+}
+
+function putThings(callback, putObject, index, id) {
+//  const settings = {
+//    url:  GROW_ENTRIES_JSON/${id},
+//    dataType: 'json',
+//    type: 'PUT',
+//    data: putObject,
+//    success: function(result) {
+//    }
+//});
+//$.ajax(settings);
+  setTimeout(function(){callback(putGrow)}, 100);
+  putGrow.splice(index, 0, putObject);
+}
+
+//function displayGrowGrid(data) {
+//  LOCAL_DATA = data;
+//  let numberOfGrows = LOCAL_DATA.grows.length;
+//  let imageGrid = renderImageGridHtml(renderImageHtml(), numberOfGrows);
+//  $('#grow-grid').html(imageGrid);
+//}
+
+//function getAndDisplayGrowGrid() {
+//  getAllGrows(displayGrowGrid);
+//}
+
+//render grid for grow-collection-page
+function renderImageGrid(item, index) {
+  let imageHtml = `
+  <figure id="img-${LOCAL_DATA.grows[index].id}" class="grow-links">
+    <div class="grid-images" style="background-image: url(${LOCAL_DATA.grows[index].image})"></div>
+    <div class="img-hover hidden">
+      <h1>${LOCAL_DATA.grows[index].growName}</h1>
+    </div>
+  </figure>
+  `
+    return imageHtml;
+}
+
+function displayImageGrid() {
+  let imageGrid = LOCAL_DATA.grows.map((item, index) => renderImageGrid(item, index));
   $('#grow-grid').html(imageGrid);
 }
 
-function getAndDisplayGrowGrid() {
-  dataRequest(displayGrowGrid, growCollection);
-}
-
-//render grid for grow-collection-page
-function renderImageHtml(item, index) {
-  let imageHtml = `
-    <figure class="col-3 img-grid">
-     <div id="img-${index}" style="background-image: url(${item.image})"></div>
-     <div class="img-hover hidden">
-       <h1>${item.growName}</h1>
-     </div>
-  </figure>
-    `
-  return (imageHtml);
-}
-
-function renderImageRowHtml(renderImage, num){
-  for (let i = 0; i == num; i++) {
-     columns += renderImage();
-  }
-  return '<div class="row">' + columns + '</div>';
-}
-
-function renderImageGridHtml(imageHtml, growNumber) {
-  let fullHtml;
-  let columns;
-  let fullRows;
-  let remainder = growNumber % 4;
-  //if the number of grow entries is less than or equal to 4, then just create a string of
-  //html with that number of images
-  if (growNumber <= 4) {
-    fullHtml = renderImageRowHtml(imageHtml, growNumber);
-  }
-  //if the number of grow enties is greater than 4 and divisible by 4, then create
-  //chunks of html for 4 images at a time, that are each wrapped in a "row" tag
-  else if (growNumber > 4 && remainder == 0) {
-    let numberOfRows = growNumber/4;
-  //make html for full rows
-    for (let i = 0; i == numberOfRows; i++) {
-      fullRows += renderImageRowHtml(imageHtml, 4);
-    }
-    fullHtml = fullRows;
-  }
-  //if the number of grow entries is greater than 4 and not divisible by 4, then
-  //the last row of html will have less than 4 images in it
-  else {
-    let numberOfFullRows = toFixed(growNumber/4);
-    //make html for full rows
-    for (let i = 0; i == numberOfRows; i++) {
-      fullRows += renderImageRowHtml(imageHtml, 4);
-    }
-    let fullHtml = fullRows + renderImageRowHtml(imageHtml, remainder);
-  }
-    return
-  }
-//inserts grid for grow-collection-page into DOM
-
-
-/////////NEED HELP with IMG GRID above ^
-
-
-
 // listens for user to select one of the grow images to display grow-entries-page
-function displayGrow() {
+function onGrowClick() {
   $().on("click", function(event) {
     let imageId = $(this).attr('id');
-    let index = Number(figureId.slice(4, figureId.length));
-    dataRequest(); //insert callback function
-    ('#grow-collection-page').addClass('hidden');
+    console.log(imageId);
+    let index = Number(imageId.slice(4, imageId.length));
+    console.log(index);
+    //let growId = LOCAL_DATA.grows[index].id;
+    //console.log(growId);
+    displayGrowTimeline(index);
+    $('#grow-collection-page').addClass('hidden');
     $('#grow-entries-page').removeClass('hidden');
-
   });
+}
+
+//data object will be all the entries for one grow
+//function renderGrowTimeline(index) {
+//  let thisGrow = LOCAL_DATA.grow[index];
+//  let firstEntryDate = moment(thisGrow.startDate);
+//  let addNewEntryDate = moment();
+//  let timeLength = addNewEntryDate - firstEntryDate;
+
+  //.page class sets the section div to 100vh
+  //header will be set to 15 vh
+  //which leaves 85 vh for the grow-timeline div
+  //the line will be an image that always has a height of 1vh,
+  //in order to maintain the proportions of the line
+  //algorithm will determine how many vertical repetitions of the
+  //line image are equal to 1 day of the timeline, based on the overall
+  //length of the timeline
+
+//}
+
+//function renderTimeline() {
+//  let timeline = ;
+//}
+
+function displayGrowTimeline(index) {
+  $('#grow-name').html(data.growName);
+  renderGrowTimeline(index);
 }
 
 //listens for user to select option to add a new grow
 function addGrow() {
-  $().on("click", function(event) {
-    ('#grow-collection-page').addClass('hidden');
+  //$().on("click", function(event) {
+    $('#grow-collection-page').addClass('hidden');
     $('#add-grow-page').removeClass('hidden');
     submitGrow();
-  });
+  //});
 }
 
 //listens for user to submit info for a new grow
@@ -108,43 +214,55 @@ function submitGrow() {
   $('#submit-grow').on("click", function(event) {
     $('#add-grow-page').addClass('hidden');
     event.preventDefault();
-    let growName = $('#grow-name').val();
+    let growName = $('#new-grow-name').val();
     let germDate = $('#germination-date').val();
     let growStrain = $('input[type=radio][name=strain]:checked').val();
-    makeApost(dataRequest)
-    dataRequest(makeApost(), growCollection);
-
+    postGrow(afterPostTest, makePostObject(growName, germDate, growStrain));
   });
 }
 
-function dataRequest(callback, dataParam) {
-
-//this function is just using a timeout function until I've built a live API,
-//when i'll change it to an AJAX call
-setTimeout(function(){callback(dataParam)}, 1);
+function makePostObject(name, date, strain) {
+  const post = {
+    id: '',
+    growName: name,
+    startDate: date,
+    endDate: '',
+    strain: strain,
+  };
+  return post;
 }
 
-function makeApost() {
-  data.grows.growName = name;
-  data.grows.startDate = date;
-  data.grows.strain = strain;
+//listens for user to select option to delete a past grow
+function deleteGrowPage() {
+  //$().on("click", function(event) {
+  $('#grow-entries-page').addClass('hidden');
+  $('#delete-entry-page').removeClass('hidden');
+  deleteGrow(index);
+  //});
 }
-
-//listens for user to selection option to delete a past grow
-function deleteGrow() {
-  $().on("click", function(event) {
-
+//listens for user to confirm their option to delete a past grow
+function deleteGrow(index) {
+  $('#delete-confirm').on("click", function(event) {
+  //deleteThings(afterDeleteTest, index);
+  deleteThings(afterDeleteTest, 0);
   });
+}
+
+function deleteThings(callback, id) {
+//  const settings = {
+//      url: GROW_ENTRIES_JSON/${id},
+//      dataType: 'json',
+//      type: 'DELETE',
+//      success: function(result) {
+//      }
+//  });
+//$.ajax(settings);
+  setTimeout(function(){callback(deleteAgrow)}, 100);
+  deleteAgrow.splice(index, 1);
 }
 
 //for grow-entries-page, where user is able to see a timeline of all the entries that have been inputed for that grow
-function getEntries(callback) {
-//makes AJAX call to server for all of the current and past grows
 
-//this function is just using a timeout function until I've built a live API,
-//when i'll change it to an AJAX call
-//setTimeout(function(){ callback(MOCK_GROW_ENTRIES)}, 1);
-}
 
 //render timeline entries on grow-entries-page
 function renderTimeline(item, index) {
@@ -170,6 +288,8 @@ function addLogEntry() {
   });
 }
 
-$(function() {
-    addGrow();
+$(function(){
+  getAllGrows(displayImageGrid);
+  //getAllGrows()
+  //.then(displayImageGrid());
 });
--- public/mock-grow-entries.js
+++ public/mock-grow-entries.js
@@ -2,6 +2,429 @@ const growCollection =
 {
   grows: [
     {
+      id: '100',
+      growName: 'Tangerine Dream',
+      startDate: '2018/01/12',
+      endDate: '',
+      strain: 'indica',
+      image: 'http://wesb.com/wp-content/uploads/2015/06/citral-cannabis-plant.jpg',
+      entries: [
+        {
+          id: '34623',
+          date: '2018/01/15',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, facer laoreet definiebas te pro. Vis cu recusabo gubergren dissentias, ea est natum facilisis, vis.'
+        },
+        {
+          id: '56734',
+          date: '2018/01/18',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '34136',
+          date: '2018/01/20',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, labitur.'
+        },
+        {
+          id: '674563',
+          date: '2018/01/23',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '456456',
+          date: '2018/01/26',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: ''
+        },
+        {
+          id: '453256',
+          date: '2018/01/29',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        }
+      ]
+    },
+    {
+      id: '200',
+      growName: 'Green Crack',
+      startDate: '2018/02/3',
+      endDate: '',
+      strain: 'sativa',
+      image: 'https://magazine.grasscity.com/wp-content/uploads/2016/02/How-To-Harvest-Outdoor-Marijuana-Plants-720x480@2x.jpg',
+      entries: [
+        {
+          id: '45645',
+          date: '2018/02/5',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, facer laoreet definiebas te pro. Vis cu recusabo gubergren dissentias, ea est natum facilisis, vis.'
+        },
+        {
+          id: '47843',
+          date: '2018/02/10',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '96785',
+          date: '2018/02/14',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, labitur.'
+        },
+        {
+          id: '07567',
+          date: '2018/02/17',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '24547',
+          date: '2018/02/20',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: ''
+        },
+        {
+          id: '45634',
+          date: '2018/02/23',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '34177',
+          date: '2018/02/25',
+          phase: 'flowering',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+          id: '45630',
+          date: '2018/02/27',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        }
+      ]
+    },
+    {
+      id: '300',
+      growName: 'Deep Sleep',
+      startDate: '2018/02/3',
+      endDate: '',
+      strain: 'indica',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/1/11/Young_cannabis_plant_in_the_vegetative_stage_01.jpg',
+      entries: [
+        {
+          id: '45645',
+          date: '2018/02/5',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, facer laoreet definiebas te pro. Vis cu recusabo gubergren dissentias, ea est natum facilisis, vis.'
+        },
+        {
+          id: '47843',
+          date: '2018/02/10',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '96785',
+          date: '2018/02/14',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, labitur.'
+        },
+        {
+          id: '07567',
+          date: '2018/02/17',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '24547',
+          date: '2018/02/20',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: ''
+        },
+        {
+          id: '45634',
+          date: '2018/02/23',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '34177',
+          date: '2018/02/25',
+          phase: 'flowering',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+          id: '45630',
+          date: '2018/02/27',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        }
+      ]
+    },
+    {
+      id: '400',
+      growName: 'Acapulco Gold',
+      startDate: '2018/02/3',
+      endDate: '',
+      strain: 'sativa',
+      image: 'https://www.growweedeasy.com/wp-content/uploads/2013/03/fat-smelly-cannabis-cola.jpg',
+      entries: [
+        {
+          id: '45645',
+          date: '2018/02/5',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, facer laoreet definiebas te pro. Vis cu recusabo gubergren dissentias, ea est natum facilisis, vis.'
+        },
+        {
+          id: '47843',
+          date: '2018/02/10',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '96785',
+          date: '2018/02/14',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, labitur.'
+        },
+        {
+          id: '07567',
+          date: '2018/02/17',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '24547',
+          date: '2018/02/20',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: ''
+        },
+        {
+          id: '45634',
+          date: '2018/02/23',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '34177',
+          date: '2018/02/25',
+          phase: 'flowering',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+          id: '45630',
+          date: '2018/02/27',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        }
+      ]
+    },
+    {
+      id: '500',
+      growName: 'Acapulco Gold',
+      startDate: '2018/02/3',
+      endDate: '',
+      strain: 'sativa',
+      image: 'https://www.growweedeasy.com/wp-content/uploads/2010/05/ultimate-supercrop-bending-lst-example.jpg',
+      entries: [
+        {
+          id: '45645',
+          date: '2018/02/5',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, facer laoreet definiebas te pro. Vis cu recusabo gubergren dissentias, ea est natum facilisis, vis.'
+        },
+        {
+          id: '47843',
+          date: '2018/02/10',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '96785',
+          date: '2018/02/14',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet, labitur.'
+        },
+        {
+          id: '07567',
+          date: '2018/02/17',
+          phase: 'vegetative',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '24547',
+          date: '2018/02/20',
+          phase: 'vegetative',
+          watered: true,
+          fed: false,
+          notes: ''
+        },
+        {
+          id: '45634',
+          date: '2018/02/23',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        },
+        {
+          id: '34177',
+          date: '2018/02/25',
+          phase: 'flowering',
+          watered: true,
+          fed: false,
+          notes: 'Lorem ipsum dolor sit amet.'
+        },
+        {
+          id: '45630',
+          date: '2018/02/27',
+          phase: 'flowering',
+          watered: false,
+          fed: true,
+          notes: ''
+        }
+      ]
+    }
+  ]
+};
+
+const oneGrow =
+{
+  id: '100',
+  growName: 'Tangerine Dream',
+  startDate: '2018/01/12',
+  endDate: '',
+  strain: 'indica',
+  image: 'https://dummyimage.com/300x300/0b8f1f/fff',
+  entries: [
+    {
+      id: '34623',
+      date: '01/16/18',
+      phase: 'vegetative',
+      watered: true,
+      fed: false,
+      notes: 'Lorem ipsum dolor sit amet, facer laoreet definiebas te pro. Vis cu recusabo gubergren dissentias, ea est natum facilisis, vis.'
+    },
+    {
+      id: '56734',
+      date: '01/20/18',
+      phase: 'vegetative',
+      watered: false,
+      fed: true,
+      notes: ''
+    },
+    {
+      id: '34136',
+      date: '01/23/18',
+      phase: 'vegetative',
+      watered: true,
+      fed: false,
+      notes: 'Lorem ipsum dolor sit amet, labitur.'
+    },
+    {
+      id: '674563',
+      date: '01/28/18',
+      phase: 'vegetative',
+      watered: false,
+      fed: true,
+      notes: ''
+    },
+    {
+      id: '456456',
+      date: '02/02/18',
+      phase: 'vegetative',
+      watered: true,
+      fed: false,
+      notes: ''
+    },
+    {
+      id: '453256',
+      date: '02/05/18',
+      phase: 'flowering',
+      watered: false,
+      fed: true,
+      notes: ''
+    }
+  ]
+};
+
+let postAgrow = {};
+
+const deleteAgrow =
+{
+  grows: [
+    {
+      id: '100',
       growName: 'Tangerine Dream',
       startDate: '2018/01/12',
       endDate: '',
@@ -59,6 +482,7 @@ const growCollection =
       ]
     },
     {
+      id: '200',
       growName: 'Green Crack',
       startDate: '01/12/18',
       endDate: '',
@@ -130,6 +554,67 @@ const growCollection =
           notes: ''
         }
       ]
+    }
+  ]
+};
+
+
+const putGrow =
+{
+  id: '100',
+  growName: 'Tangerine Dream',
+  startDate: '2018/01/12',
+  endDate: '',
+  strain: 'indica',
+  image: 'https://dummyimage.com/300x300/0b8f1f/fff',
+  entries: [
+    {
+      id: '34623',
+      date: '01/16/18',
+      phase: 'vegetative',
+      watered: true,
+      fed: false,
+      notes: 'Lorem ipsum dolor sit amet, facer laoreet definiebas te pro. Vis cu recusabo gubergren dissentias, ea est natum facilisis, vis.'
+    },
+    {
+      id: '56734',
+      date: '01/20/18',
+      phase: 'vegetative',
+      watered: false,
+      fed: true,
+      notes: ''
+    },
+    {
+      id: '34136',
+      date: '01/23/18',
+      phase: 'vegetative',
+      watered: true,
+      fed: false,
+      notes: 'Lorem ipsum dolor sit amet, labitur.'
+    },
+    {
+      id: '674563',
+      date: '01/28/18',
+      phase: 'vegetative',
+      watered: false,
+      fed: true,
+      notes: ''
+    },
+    {
+      id: '456456',
+      date: '02/02/18',
+      phase: 'vegetative',
+      watered: true,
+      fed: false,
+      notes: ''
+    },
+    {
+      id: '453256',
+      date: '02/05/18',
+      phase: 'flowering',
+      watered: false,
+      fed: true,
+      notes: ''
     }
   ]
 };
