f76fe65eb3dbb5758735b4e109109f96adfec49b
Bibi <bibi.samreny@protonmail.com>
2018-05-26 16:12:18 -0700

    first callback changed to a promise

--- public/app.js
+++ public/app.js
@@ -1,59 +1,42 @@
 const GROW_ENTRIES_JSON = 'mock-grow-entries.json';
 let LOCAL_DATA = {};
 let CURRENT_GROW = {};
-let GRID_TRACKER = 0;
+let gridTracker = 0;
 
+//change my lets to const
 //consolidate ^these into 1 object
 
-//function getAllGrows() {
-//  return new Promise ((resolve, reject) => {
-//    const settings = {
+function getAllGrows() {
+  return new Promise ((resolve, reject) => {
+    LOCAL_DATA = growCollection;
+    //    const settings = {
     //    url: GROW_ENTRIES_JSON,
     //    dataType: 'json',
     //    type: 'GET',
     //    success: callback
     //  };
     //  $.ajax(settings);
-//    timeout(100).then
-//    resolve("success!");//fulfilled
-//    reject("total failure");//rejected
-//  });
-//}
 
-
-//function getAllGrows(callback) {
-//  const settings = {
-//    url: GROW_ENTRIES_JSON,
-//    dataType: 'json',
-//    type: 'GET',
-//    success: callback
-//  };
-//  $.ajax(settings);
-//setTimeout(function(){callback(growCollection)}, 100);
-//LOCAL_DATA = growCollection;
-//}
-
-function getAllEntries(callback) {
-//  const settings = {
-//    url: GROW_ENTRIES_JSON,
-//    dataType: 'json',
-//    type: 'GET',
-//    success: callback
-//  };
-//  $.ajax(settings);
-setTimeout(function(){callback(growCollection)}, 100);
+    resolve("success!");//fulfilled
+    reject("total failure");//rejected
+  });
 }
 
 //flip the callback and id arguments
 function getGrow(callback, id) {
-  //  const settings = {
-  //    url: GROW_ENTRIES_JSON/${id},
-  //    dataType: 'json',
-  //    type: 'GET',
-  //    success: callback
-  //  };
-  //  $.ajax(settings);
-  setTimeout(function(){callback(oneGrow)}, 100);
+
+  return new Promise ((resolve, reject) => {
+    //  const settings = {
+    //    url: GROW_ENTRIES_JSON/${id},
+    //    dataType: 'json',
+    //    type: 'GET',
+    //    success: callback
+    //  };
+    //  $.ajax(settings);
+    setTimeout(function(){callback(oneGrow)}, 100);
+    resolve("success!");
+    reject("total failure");
+  })
 }
 
 //flip the callback and id arguments
@@ -143,7 +126,7 @@ function putThings(callback, putObject, index, id) {
 //render grid for grow-collection-page
 function renderImageGrid(item, index) {
   let imageHtml = `
-  <figure id="img-${LOCAL_DATA.grows[index].id}" class="grow-links">
+  <figure id="img-${index}" class="grow-links">
     <div class="grid-images" style="background-image: url(${LOCAL_DATA.grows[index].image})"></div>
     <div class="img-hover hidden">
       <h1>${LOCAL_DATA.grows[index].growName}</h1>
@@ -155,48 +138,40 @@ function renderImageGrid(item, index) {
 
 function displayImageGrid() {
   let imageGrid = LOCAL_DATA.grows.map((item, index) => renderImageGrid(item, index));
-  $('#grow-grid').html(imageGrid);
+  document.getElementById("grow-grid").innerHTML = imageGrid;
 }
 
 // listens for user to select one of the grow images to display grow-entries-page
 function onGrowClick() {
-  $().on("click", function(event) {
-    let imageId = $(this).attr('id');
-    console.log(imageId);
-    let index = Number(imageId.slice(4, imageId.length));
-    console.log(index);
-    //let growId = LOCAL_DATA.grows[index].id;
-    //console.log(growId);
-    displayGrowTimeline(index);
+  let currentGrow = document.querySelector('.grow-links');
+  currentGrow.addEventListener('click', function () {
+    const imageId = this.getAttribute('id');
+    let imageIndex= Number(imageId.slice(4, imageId.length));
+    let growId = LOCAL_DATA.grows[index].id;
+    getGrow(callback, id)
+    displayGrowTimeline(imageIndex);
     $('#grow-collection-page').addClass('hidden');
     $('#grow-entries-page').removeClass('hidden');
   });
 }
 
-//data object will be all the entries for one grow
 //function renderGrowTimeline(index) {
-//  let thisGrow = LOCAL_DATA.grow[index];
+//  let thisGrow = LOCAL_DATA.grows[];
 //  let firstEntryDate = moment(thisGrow.startDate);
 //  let addNewEntryDate = moment();
 //  let timeLength = addNewEntryDate - firstEntryDate;
-
-  //.page class sets the section div to 100vh
-  //header will be set to 15 vh
-  //which leaves 85 vh for the grow-timeline div
-  //the line will be an image that always has a height of 1vh,
-  //in order to maintain the proportions of the line
-  //algorithm will determine how many vertical repetitions of the
-  //line image are equal to 1 day of the timeline, based on the overall
-  //length of the timeline
-
-//}
-
-//function renderTimeline() {
-//  let timeline = ;
+//  let timelineHeight = document.getElementById('img-${index}').clientHeight;
+//  let lineUnitHeight = timelineHeight/timeLength;
+//average time of growth = 3-5 months (90-150 days)
+  //the line will be made up of a vertically repeated vector image of a
+  //vertical rectangle that represents 1 day of time
+  //the height of the line unit will be determined by the algorithm
+  //
 //}
 
 function displayGrowTimeline(index) {
-  $('#grow-name').html(data.growName);
+  document.getElementById("grow-name").innerHTML = data.growName;
+
   renderGrowTimeline(index);
 }
 
@@ -214,9 +189,9 @@ function submitGrow() {
   $('#submit-grow').on("click", function(event) {
     $('#add-grow-page').addClass('hidden');
     event.preventDefault();
-    let growName = $('#new-grow-name').val();
-    let germDate = $('#germination-date').val();
-    let growStrain = $('input[type=radio][name=strain]:checked').val();
+    const growName = $('#new-grow-name').val();
+    const germDate = $('#germination-date').val();
+    const growStrain = $('input[type=radio][name=strain]:checked').val();
     postGrow(afterPostTest, makePostObject(growName, germDate, growStrain));
   });
 }
@@ -289,7 +264,10 @@ function addLogEntry() {
 }
 
 $(function(){
-  getAllGrows(displayImageGrid);
-  //getAllGrows()
-  //.then(displayImageGrid());
+  //consolidate in 1 function: 1. make data call, 2. display info, 3. call event listeners
+  getAllGrows().then(function(){
+    return displayImageGrid();
+  }).then(function(){
+    return onGrowClick();
+  });
 });
