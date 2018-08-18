4eac3d1df8f0a4405ae982a5e23a3910f6813152
Bibi <bibi.samreny@protonmail.com>
2018-08-17 21:55:37 -0700

    changed test_database_url

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
 /*jshint esversion: 6*/
+const TEST_DATABASE_URL = 'mongodb://<bibiqiqi>:<S1ybonos>@ds123532.mlab.com:23532/weed-grow-app-test';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/weed-grow';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-weed-grow';
 exports.PORT = process.env.PORT || 8080;
