54b1fa3be0cc62538648e056249805e082dc9c76
Bibi <bibi.samreny@protonmail.com>
2018-08-17 22:58:15 -0700

    heroku deploy attempt

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
 /*jshint esversion: 6*/
+
 require('dotenv').config()
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/weed-grow';
 exports.TEST_DATABASE_URL = process.env.MLAB_TEST_DATABASE_URL || 'mongodb://localhost/test-weed-grow';
